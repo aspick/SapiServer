2685066c541dd5787f0b5ba78d75ad1b539f7f26
Yugo Terada <yugo@18th-lab.com>
2016-01-27 14:22:11 +0900

    fix
    fix wrong register script

--- register_service.ps1
+++ register_service.ps1
@@ -1,7 +1,7 @@
 go build app\server.go
 
 $Command = {
-New-Service -Name CioAudioWorker -BinaryPathName 'C:\Program Files (x86)\Windows Resource Kits\Tools\srvany.exe'
+New-Service -Name SapiServer -BinaryPathName 'C:\Program Files (x86)\Windows Resource Kits\Tools\srvany.exe'
 New-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SapiServer\Parameters'
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SapiServer\Parameters' -Name Application '$PSScriptRoot\app\server.exe'
 Write-Host '`Start-Service SapiServer` to start service'
