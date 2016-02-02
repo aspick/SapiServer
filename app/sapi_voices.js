2f0196bc8fe5aeb814aa7beb26cb6d0d492c5e71
Yugo Terada <yugo@18th-lab.com>
2016-02-02 15:30:18 +0900

    add more attribute

--- app/sapi_voices.js
+++ app/sapi_voices.js
@@ -11,6 +11,9 @@ for(var i = 0; i < voices.Count; i++){
     WScript.StdOut.Write("\"name\":\"" + voice.GetAttribute("name") + "\", ");
     WScript.StdOut.Write("\"gender\":\"" + voice.GetAttribute("gender") + "\", ");
     WScript.StdOut.Write("\"language\":\"" + voice.GetAttribute("language") + "\", ");
+    WScript.StdOut.Write("\"vendor\":\"" + voice.GetAttribute("vendor") + "\", ");
+    WScript.StdOut.Write("\"age\":\"" + voice.GetAttribute("age") + "\", ");
     WScript.StdOut.Write("\"description\":\"" + voice.GetDescription() + "\"} ");
+
 }
 WScript.StdOut.Write("]");
