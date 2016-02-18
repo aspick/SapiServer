c1277fa8d4311194746c65a1d32f075802212470
Yugo Terada <yugo@18th-lab.com>
2016-02-18 12:22:00 +0900

    fix register script

--- register_service.ps1
+++ register_service.ps1
@@ -3,7 +3,7 @@ go build -o app\server.exe app\server.go
 $Command = {
 New-Service -Name SapiServer -BinaryPathName 'C:\Program Files (x86)\Windows Resource Kits\Tools\srvany.exe'
 New-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SapiServer\Parameters'
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SapiServer\Parameters' -Name Application '$PSScriptRoot\app\server.exe'
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SapiServer\Parameters' -Name Application "$($PSScriptRoot)\app\server.exe"
 Write-Host '`Start-Service SapiServer` to start service'
 Write-Host '`sc.exe delete SapiServer` to delete service'
 }
